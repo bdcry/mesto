9a53d826529adca21e56e1785b35953b6768ae95
Tim <70958412+bdcry@users.noreply.github.com>
2022-01-29 20:27:13 +0300

    review 1

---
+++ jscripts/index.js
@@ -0,0 +1,37 @@
+const popupElement = document.querySelector('.popup'); //переменная всего popup
+const editProfile = document.querySelector('.profile-edit'); //переменная открытия редактирования профиля
+const closeButton = document.querySelector('.popup__close-button'); //переменная закрытия редактирования профиля
+
+const userName = document.querySelector('.profile__user-name'); //переменная имени профиля на странице
+const userJop = document.querySelector('.profile__user-jop'); //переменная работы на странице
+
+let nameInput = document.querySelector('.popup__input_name'); //Воспользуйтесь инструментом .querySelector()
+let jobInput = document.querySelector('.popup__input_jop'); //Воспользуйтесь инструментом .querySelector()
+
+let formElement = document.querySelector('.popup__form'); //Находим форму в DOM
+
+
+function openPopup() {
+  popupElement.classList.add('popup_opened');
+  nameInput.value = userName.textContent;
+  jobInput.value = userJop.textContent;
+}
+
+function closePopup() {
+  popupElement.classList.remove('popup_opened');
+}
+
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+
+  userName.textContent = nameInput.value; //Получите значение полей из свойства value
+  userJop.textContent = jobInput.value;
+
+  closePopup();
+}
+
+formElement.addEventListener('submit', formSubmitHandler); //Прикрепляем обработчик к форме: он будет следить за событием “submit” - «отправка»
+
+editProfile.addEventListener('click', openPopup);
+closeButton.addEventListener('click', closePopup);
